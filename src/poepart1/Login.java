387f2ce3f2a3c1fbc59d03210ad47d1efa842b28
Student <Student@LabVM2049939.31uduukmeasupejcrd5aed4ymc.jnbx.internal.cloudapp.net>
2026-09-18 09:38:20 +0200

    new method added

--- src/poepart1/Login.java
+++ src/poepart1/Login.java
@@ -29,4 +29,8 @@ public class Login extends Registration {
     boolean registrationUser(String name, String password, String cellphone) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
+    boolean checkCellPhone(String cellphone) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
